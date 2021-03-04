f6418b6d04845dde49e11fe1bef4ee68b4735408
yoonhi <13049277922@163.com>
2021-03-04 20:36:26 +0800

    Update code.php
    tellAll -> TellAll

--- part1/chapter1/1.3.1单一职责原则/code.php
+++ part1/chapter1/1.3.1单一职责原则/code.php
@@ -1,1 +1,14 @@
-<?phpclass tellAll {	private $userAgent;	public function __construct() {		$this->userAgent = $_SERVER['HTTP_USER_AGENT'];		echo $this->userAgent;	}}new tellAll();// echo// Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36
+<?php
+
+class TellAll {
+	private $userAgent;
+	public function __construct() {
+		$this->userAgent = $_SERVER['HTTP_USER_AGENT'];
+		echo $this->userAgent;
+	}
+}
+
+new tellAll();
+
+// echo
+// Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36
